20d1eb48bb457efe6b276737702913e43e76ac8a
syed khizaruddin <jarvis@syeds-MacBook-Air.local>
2021-01-08 11:47:03 +0530

    learning forms in vue

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="setActiveOption('poor')">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="setActiveOption('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'great' }">
+      <button type="button" @click="setActiveOption('great')">Great</button>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  props: ["modelValue"],
+  emits: ["update:modelValue"],
+  methods: {
+    setActiveOption(option) {
+      this.$emit("update:modelValue", option);
+    },
+  },
+};
+</script>
+<style scoped>
+.active {
+  border-color: rgb(248, 2, 248);
+}
+
+.active button {
+  color: rgb(248, 2, 248);
+}
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+button {
+  font: inherit;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+}
+</style>
